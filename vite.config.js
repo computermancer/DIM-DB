f1773106ce56032e81359c9ddc8a688c56bb3863
computermancer <bravo.mateo@gmail.com>
2025-04-28 01:16:39 -0700

    ugh6

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: './',
   publicDir: 'public',
   build: {
     outDir: 'build',
     sourcemap: true,
     rollupOptions: {
-      input: {
-        main: './src/index.jsx'
-      },
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
+        manualChunks: undefined,
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
       }
     }
   },
